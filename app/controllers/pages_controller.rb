e76c07d1a0bba3937ffb3761f0f68d7d786990cf
Helen Lam <helen.f.lam100@gmail.com>
2014-11-18 15:09:51 -0800

    initial commit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,13 @@
 class PagesController < ApplicationController
+  def verse_2
 
+  end
+
+  def verse_3
+
+  end
+
+  def verse_4
+
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#index"
+  get 'verse-2' => "pages#verse_2", as: :verse_2
+  get 'verse-3' => "pages#verse_3", as: :verse_3
+  get 'verse-4' => "pages#verse_4", as: :verse_4
 end
